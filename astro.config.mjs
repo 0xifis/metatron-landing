cac8f29984b8e849b9afc180882ef2110f74f1c8
Vishnu R Menon <vishthemenon@gmail.com>
2024-02-09 17:11:07 +0100

    Fix yarn install of heroicons package

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon({
+    include: {
+      heroicons: ["*"], // (Default) Loads entire Material Design Icon set
+    },
+  })],
   output: "server",
   adapter: netlify()
 });
